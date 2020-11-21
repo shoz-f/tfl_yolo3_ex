0ca8f7fc0af43b5e062de43a59d21d138974ba63
shozo fukuda <fukuda_shozo@yahoo.co.jp>
2020-11-21 09:26:54 +0900

    revised expects since the introduction of rounding boxes in Box::put_json().

--- test/tfl_yolo3_test.exs
+++ test/tfl_yolo3_test.exs
@@ -5,16 +5,16 @@ defmodule TflYolo3Test do
   test "object detection in 'dog.jpg'" do
     {:ok, res} = TflYolo3.TflInterp.predict("test/dog.jpg")
     assert Map.keys(res) == ["bicycle", "dog", "truck"]
-    assert res["bicycle"] == [[163, 115, 566, 435]]
-    assert res["dog"    ] == [[123, 223, 322, 543]]
-    assert res["truck"  ] == [[472,  86, 692, 166]]
+    assert res["bicycle"] == [[164, 115, 567, 435]]
+    assert res["dog"    ] == [[124, 224, 323, 544]]
+    assert res["truck"  ] == [[472,  86, 692, 167]]
   end
   
   test "object detection in 'person.jpg'" do
     {:ok, res} = TflYolo3.TflInterp.predict("test/person.jpg")
     assert Map.keys(res) == ["dog", "horse", "person"]
-    assert res["dog"   ] == [[ 61, 262, 204, 351]]
-    assert res["horse" ] == [[396, 137, 612, 351]]
-    assert res["person"] == [[189,  90, 277, 387]]
+    assert res["dog"   ] == [[ 62, 262, 204, 352]]
+    assert res["horse" ] == [[396, 137, 613, 352]]
+    assert res["person"] == [[190,  90, 277, 388]]
   end
 end
