51c1c86592699da64b6e85ca43abbd4a61f7f412
shozo fukuda <fukuda_shozo@yahoo.co.jp>
2020-12-12 13:44:45 +0900

    reduce router

--- lib/tfl_yolo3/router.ex
+++ lib/tfl_yolo3/router.ex
@@ -21,18 +21,12 @@ defmodule TflYolo3.Router do
     |> send_file(200, @photo_file)
   end
 
-  get "/result.jpg" do
-    conn
-    |> put_resp_header("content-type", "image/jpeg")
-    |> send_file(200, @result_file)
-  end
-
   post "/predict_photo" do
     with \
       :ok <- File.cp(conn.params["photo"].path, @photo_file),
       {:ok, ans} <- TflInterp.predict(@photo_file)
     do
-      result_photo(ans)
+      IO.inspect(ans)
       send_resp(conn, 200, Jason.encode!(%{"ans" => ans}))
     else
       _ ->
@@ -43,19 +37,5 @@ defmodule TflYolo3.Router do
   match _ do
     IO.inspect(conn)
     send_resp(conn, 404, "Oops!")
-  end
-  
-  def result_photo(ans) do
-    boxes =
-      Map.values(ans)
-      |> Enum.reduce([], fn x,acc -> acc ++ x end)
-
-    with photo <- CImg.create(@photo_file)
-    do
-      Enum.each(boxes, fn
-        [x0,y0,x1,y1] -> CImg.draw_box(photo, x0, y0, x1, y1, {255,0,0})
-      end)
-      CImg.save(photo, @result_file)
-    end
   end
 end
